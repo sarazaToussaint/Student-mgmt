8a58f96c2d7983afc0f077c32a523c45b66ad00d
sarazaToussaint <tmsaraza@gmail.com>
2023-01-22 19:59:39 +0200

    Populate the students table in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Student.create(
+  first_name: 'John',
+  last_name: 'Doe',
+  email: 'johndoe@gmail.com'
+)
