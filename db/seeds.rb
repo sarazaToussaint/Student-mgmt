97bf8f476403744cd96e2346be68045575b45687
sarazaToussaint <tmsaraza@gmail.com>
2023-01-22 20:14:24 +0200

    Populate the students table in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@ Student.create(
   last_name: 'Doe',
   email: 'johndoe@gmail.com'
 )
+
+20.times do |i|
+  puts "Creating Student #{i+1}"
+
+  Student.create(
+    first_name: "Student #{i+1}",
+    last_name: "Lname #{i+1}",
+    email: "Student#{i+1}@bootcamp.com"
+  )    
+end
