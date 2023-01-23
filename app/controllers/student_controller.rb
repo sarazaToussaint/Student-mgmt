eb3b75ad0e32085b1f7fcecca786c1746ecb51f1
sarazaToussaint <tmsaraza@gmail.com>
2023-01-23 18:08:43 +0200

    Make some changes

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -8,4 +8,13 @@ class StudentController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(
+      first_name: params[:student][:first_name],
+      last_name: params[:student][:last_name],
+      email: params[:student][:email]
+    )
+    @student.save
+  end
+
 end
