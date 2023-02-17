9211e091549d2b1dfb66cc29076e895d7e76c134
sarazaToussaint <tmsaraza@gmail.com>
2023-02-17 20:13:08 +0200

    Modify a controller student file

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(
+      first_name: params[:student][:first_name],
+      last_name: params[:student][:last_name],
+      email: params[:student][:email]
+    )
+    @student.save
+  end
+end
