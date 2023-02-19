e61ee9871b69bac49a3bea54d09514b76f380b42
sarazaToussaint <tmsaraza@gmail.com>
2023-02-19 08:43:28 +0200

    Remove the unused code in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "students#new"
-  get "/students", to: "students#new"
+  root "welcome#index"
+  # get "/students", to: "students#new"
 
   resources :students
 
