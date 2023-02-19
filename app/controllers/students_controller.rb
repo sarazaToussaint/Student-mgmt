ac1e1791d96e74ff9ef72fa6db6ff774a52c691c
sarazaToussaint <tmsaraza@gmail.com>
2023-02-19 10:27:50 +0200

    Add Params to the student's create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,15 +12,17 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(
-      first_name: params[:student][:first_name],
-      last_name: params[:student][:last_name],
-      email: params[:student][:email]
-    )
+    @student = Student.new(student_params)
     if @student.save
       redirect_to students_path
     else
       render :new
     end       
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
   end
 end
