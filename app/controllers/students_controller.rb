34b3005ae98b1e0417aeb3461934b0f440ec2c72
sarazaToussaint <tmsaraza@gmail.com>
2023-02-20 13:26:25 +0200

    Refact the student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,15 +19,12 @@ class StudentsController < ApplicationController
   end
 
   def show
-    # @student = Student.find(params[:id])
   end
 
   def edit
-    # @student = Student.find(params[:id])
   end
 
   def update
-    # @student = Student.find(params[:id])
     if @student.update(student_params)
       redirect_to student_path(@student)
     else
