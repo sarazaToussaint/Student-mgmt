2fd1b57652cbe55b34759a847cb9b31d5647964c
sarazaToussaint <tmsaraza@gmail.com>
2023-02-21 13:54:01 +0200

    Add the callback action in the student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
+
+  before_create :display_greetings
+
+  def display_greetings
+    put "Hello the Ruby on Rails community"
+  end
 end
