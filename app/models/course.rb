4ce19134d19084e7a19652d02d7314acf82ffeb0
sarazaToussaint <tmsaraza@gmail.com>
2023-02-22 18:29:18 +0200

    Make some changes on the create before action msg

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,6 @@ class Course < ApplicationRecord
     validates :name, :description, presence: true
 
     def display_greetin
-      puts "Hello the Ruby on Rails community"
+      puts "Hello the Ruby on Rails"
     end
 end
