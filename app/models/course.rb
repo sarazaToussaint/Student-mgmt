07aa97e41e3261e406facfa6ad7fedc34d868898
sarazaToussaint <tmsaraza@gmail.com>
2023-02-22 18:28:48 +0200

    Make some changes on the create before action

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     validates :name, :description, presence: true
 
-    def display_greetings
+    def display_greetin
       puts "Hello the Ruby on Rails community"
     end
 end
