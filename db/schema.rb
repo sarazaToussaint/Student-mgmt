ea16a7b413087f6d9c438efddeeb8699ecac8543
sarazaToussaint <tmsaraza@gmail.com>
2023-03-01 11:23:06 +0200

    Destroy all the files generating by scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_151423) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_091334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "courses", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
