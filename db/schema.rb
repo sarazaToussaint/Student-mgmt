cb53d6f4b808e2720ff7968ca53a288b6e3b1391
sarazaToussaint <tmsaraza@gmail.com>
2023-03-01 16:04:51 +0200

    Remove column from the student table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_093713) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_135138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_093713) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "dob"
+    t.string "address"
+    t.string "contact"
   end
 
 end
