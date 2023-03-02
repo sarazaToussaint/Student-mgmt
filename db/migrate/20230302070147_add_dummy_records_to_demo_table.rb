4e549bac17f71552a00190012bda4e7bc14dcae1
sarazaToussaint <tmsaraza@gmail.com>
2023-03-02 09:11:42 +0200

    Generate a migration file to add records

--- db/migrate/20230302070147_add_dummy_records_to_demo_table.rb
+++ db/migrate/20230302070147_add_dummy_records_to_demo_table.rb
@@ -1,5 +1,11 @@
 class AddDummyRecordsToDemoTable < ActiveRecord::Migration[7.0]
   def change
-    
+    10.times do |i|
+      puts "Creating Demo #{i+1}"
+      Demo.create(
+        name: "Demo name #{i+1}",
+        description: "Demo Description #{i+1}"
+      )
+    end
   end
 end
