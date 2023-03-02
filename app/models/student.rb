f42b00f75df7f8d732f6bf673724a69979f8ad1b
sarazaToussaint <tmsaraza@gmail.com>
2023-03-02 17:33:16 +0200

    Add association between blog and studnt

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,11 @@
 class Student < ApplicationRecord
+  has_many :blogs
+
   validates :first_name, :last_name, :email, presence: true
 
-  before_create :display_greetings
+  # before_create :display_greetings
 
-  def display_greetings
-    puts "Hello the Ruby on Rails community"
-  end
+  # def display_greetings
+  #   puts "Hello the Ruby on Rails community"
+  # end
 end
