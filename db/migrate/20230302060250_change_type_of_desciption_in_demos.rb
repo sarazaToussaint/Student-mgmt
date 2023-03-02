d738a0375f3857509e22d452585710cc44d50b47
sarazaToussaint <tmsaraza@gmail.com>
2023-03-02 08:39:21 +0200

    Use of up and down methods to change colmn type

--- db/migrate/20230302060250_change_type_of_desciption_in_demos.rb
+++ db/migrate/20230302060250_change_type_of_desciption_in_demos.rb
@@ -1,13 +1,21 @@
 class ChangeTypeOfDesciptionInDemos < ActiveRecord::Migration[7.0]
-  def change
-    reversible do |dir|
-      dir.up do 
-        change_column :demos, :description, :text
-      end
-
-      dir.down do
-        change_column :demos, :description, :string
-      end
+  def up
+    change_column :demos, :description, :text
   end
- end
+
+  def down
+    change_column :demos, :description, :string
+  end
+
+#   def change
+#     reversible do |dir|
+#       dir.up do 
+#         change_column :demos, :description, :text
+#       end
+
+#       dir.down do
+#         change_column :demos, :description, :string
+#       end
+#   end
+#  end
 end
