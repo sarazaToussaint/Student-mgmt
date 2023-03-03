30b5955386c529fa4747641e98d63d4c3d0c02b9
sarazaToussaint <tmsaraza@gmail.com>
2023-03-03 21:56:00 +0200

    Make some changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student < ApplicationRecord
   has_many :blogs
 
   validates :first_name, :last_name, :email, presence: true
+
+  # before_create :display_greetings
+
+  # def display_greetings
+  #   puts "Hello the Ruby on Rails community"
+  # end
 end
