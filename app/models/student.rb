5472a326fb84c3511770d68b9847a2c520e5abcf
sarazaToussaint <tmsaraza@gmail.com>
2023-03-03 21:55:37 +0200

    Remove the unused codes

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,10 +2,4 @@ class Student < ApplicationRecord
   has_many :blogs
 
   validates :first_name, :last_name, :email, presence: true
-
-  # before_create :display_greetings
-
-  # def display_greetings
-  #   puts "Hello the Ruby on Rails community"
-  # end
 end
