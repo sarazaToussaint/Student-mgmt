05161893973daf1b3ab17fbedac109ff92ce889e
sarazaToussaint <tmsaraza@gmail.com>
2023-03-03 21:59:44 +0200

    Remove the unused codes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
 
-  # GET /courses or /courses.json
+  # GET /courses
   def index
     @courses = Course.all
   end
