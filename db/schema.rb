8a58de2b8074ccc1743cb51fbb8ff2fd53619dd5
sarazaToussaint <tmsaraza@gmail.com>
2023-03-05 09:17:01 +0200

    Migrate the project table into the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_203833) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_071339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_203833) do
   end
 
   create_table "demos", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
