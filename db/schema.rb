bb0fdce849852f3cc9f8cd4f125caffc1ff8e59c
sarazaToussaint <tmsaraza@gmail.com>
2023-03-05 15:32:43 +0200

    Add a submition date column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_091110) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_130702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_05_091110) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "submission_data"
+    t.date "submission_date"
     t.index ["project_id"], name: "index_student_projects_on_project_id"
     t.index ["student_id"], name: "index_student_projects_on_student_id"
   end
