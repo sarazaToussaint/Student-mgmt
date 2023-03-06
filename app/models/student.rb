1069399d045c388eb7ac4d6975d7069c13b5fdb8
sarazaToussaint <tmsaraza@gmail.com>
2023-03-06 17:09:06 +0200

    Fix typos errors on all model classes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   has_many :blogs
-  has_and_belongs_to_many :course
+  has_and_belongs_to_many :courses
   has_many :student_projects
   has_many :students, through: :student_projects
 
