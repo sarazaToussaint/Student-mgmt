cae2e97eca5d1983e9eb0e4841c0fbaa012c320a
sarazaToussaint <tmsaraza@gmail.com>
2023-03-08 23:43:26 +0200

    Remove a callback method to the demo model

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,10 +1,3 @@
 class Demo < ApplicationRecord
   has_one :sub_demo
-
-  before_create :greeting_message
-
-  def greeting_message
-    puts "Hey! I will run before you create an object!"
-  end
-
 end
