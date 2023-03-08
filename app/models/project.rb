1eff952b67596978d0d4aa47fba9fe546ae8659f
sarazaToussaint <tmsaraza@gmail.com>
2023-03-08 23:44:00 +0200

    Remove a callback method to the project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,4 @@
 class Project < ApplicationRecord
   has_many :student_projects
   has_many :students, through: :student_projects
-
-
-  before_create :display_greetings
-
-  def display_greetings
-    puts "Hello the Ruby on Rails community"
-  end
 end
