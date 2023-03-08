302294170716c3fa3426bd42bafe58ead65ba870
sarazaToussaint <tmsaraza@gmail.com>
2023-03-08 15:56:19 +0200

    Add a callback method to demo model

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,9 +1,9 @@
 class Demo < ApplicationRecord
   has_one :sub_demo
 
-  # before_create :greeting_message
+  before_create :greeting_message
 
-  before_create do
+  def greeting_message
     puts "Hey! I will run before you create an object!"
   end
 
