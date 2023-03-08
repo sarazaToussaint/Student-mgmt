da9bb71f0eae3fd7e7cedac38e177c46a01e45b8
sarazaToussaint <tmsaraza@gmail.com>
2023-03-08 23:37:23 +0200

    Add a callback method to the course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,11 @@
 class Course < ApplicationRecord
  validates :name, :description, presence: true
  has_and_belongs_to_many :students
+
+
+ before_create :display_greetings
+
+ def display_greetings
+   puts "Hello the Ruby on Rails community"
+ end
 end
