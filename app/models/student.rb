246a1a29f1f82f41d3da52ae41213a1e4c82f639
sarazaToussaint <tmsaraza@gmail.com>
2023-03-09 08:17:48 +0200

    Add a callback method to the student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,8 @@ class Student < ApplicationRecord
   after_create :display_student_age
 
   def display_student_age
-   if date_of_birth.present?
-    age = Date.today.year - date_of_birth.year
+   if self.date_of_birth.present?
+    age = Date.today.year - self.date_of_birth.year
     puts "==========Age of the student is #{age}============="
    else
     puts "========Age can not be calculated without date_of_birth=========="
