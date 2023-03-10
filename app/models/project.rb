a5ddfb2ada86512582f9c5b993568d1bc900918c
sarazaToussaint <tmsaraza@gmail.com>
2023-03-10 23:27:41 +0200

    Make changes in the project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 20, maximum: 1000}
+
   has_many :student_projects
   has_many :students, through: :student_projects
 end
