804dd5803e3a3278ccb9de6c23f728cecd684645
sarazaToussaint <tmsaraza@gmail.com>
2023-03-10 23:26:10 +0200

    Make changes in the student_project model

--- app/models/student_project.rb
+++ app/models/student_project.rb
@@ -1,8 +1,4 @@
 class StudentProject < ApplicationRecord
-  validates :name, :description, presence: true
-  validates :name, uniqueness: true
-  validates :description, length: { minimum: 20, maximum: 1000}
-
   belongs_to :student
   belongs_to :project
 end
