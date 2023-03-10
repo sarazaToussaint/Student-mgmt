8dd8eba0b5470f38967abfb9c4a08f893d380685
sarazaToussaint <tmsaraza@gmail.com>
2023-03-10 23:27:01 +0200

    Make changes in the sub_demo model

--- app/models/sub_demo.rb
+++ app/models/sub_demo.rb
@@ -1,7 +1,3 @@
 class SubDemo < ApplicationRecord
-  validates :name, :description, presence: true
-  validates :name, uniqueness: true
-  validates :description, length: { minimum: 20, maximum: 1000}
-
   belongs_to :demo
 end
