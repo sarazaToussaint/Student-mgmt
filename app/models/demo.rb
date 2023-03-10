61dbd40dbbd363f1ae14dd9f301e6ef724692fb5
sarazaToussaint <tmsaraza@gmail.com>
2023-03-10 23:23:34 +0200

    Make changes in the demo model

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,3 +1,7 @@
 class Demo < ApplicationRecord
+  validates :name, :description, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 20, maximum: 1000}
+
   has_one :sub_demo
 end
