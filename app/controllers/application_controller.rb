6db7dba269e172ec459ef43befe0cdff3e6b784e
sarazaToussaint <tmsaraza@gmail.com>
2023-03-11 16:42:58 +0200

    Add the before action in the application controllr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :print_display_message
-
+    
+    private
+    
     def print_display_message
       puts "I'm an Inherited before action filter"
     end
