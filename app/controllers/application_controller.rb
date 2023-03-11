9b2370ca11386e3d817171d0656a3fd88be45b9b
sarazaToussaint <tmsaraza@gmail.com>
2023-03-11 16:48:48 +0200

    Add the after action in the application controllr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
     before_action :print_display_message
+
+    after_action :after_display_message
     
     private
-    
+
     def print_display_message
       puts "I'm an Inherited before action filter"
+    end
+
+    def after_display_message
+      puts "==========================================="
+      puts "This is an Inherited after filter message"
+      puts "==========================================="
     end
 
 end
