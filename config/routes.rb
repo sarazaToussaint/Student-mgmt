f959da6ca8dbffd59f6c6474814d6bd457bc7840
sarazaToussaint <tmsaraza@gmail.com>
2023-03-12 18:35:21 +0200

    Remove the memeber route in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,10 @@ Rails.application.routes.draw do
   resources :users do
     member do
       get :preview_profile
-      get :preview_contact
     end
 
     collection do
       get :view_users
-      get :delete_user
     end
   end
 
