a95e2da66462252d7a5247b60fbe9fd8faae0eaf
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 20:47:07 +0200

    Remove the unused codes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,4 @@
 class Project < ApplicationRecord
-  # validates :name, presence: true, uniqueness: true
-  # validates :description, presence: true, length: { minimum: 10, maximum: 1000}
   include Validatable
 
   has_many :student_projects
