942af67b7c789954fc91f29daec9a80713a1af5b
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 11:35:15 +0200

    Remove all the concerns in models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ApplicationRecord
-  # validates :name, presence: true, uniqueness: true
-  # validates :description, presence: true, length: { minimum: 10, maximum: 1000}
-  include Validatable
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000}
 
   has_many :student_projects
   has_many :students, through: :student_projects
