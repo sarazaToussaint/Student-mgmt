8a28e42eaf6265467cd19e0f2b0d13c5c3474b86
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 11:35:04 +0200

    Remove all the concerns in models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
-class Course < ApplicationRecor
-  include Validatable   
+class Course < ApplicationRecord
+ validates :name, :description, presence: true
+ validates :name, uniqueness: true
+ validates :description, length: { minimum: 10, maximum: 1000}
 
-  has_and_belongs_to_many :students
+ has_and_belongs_to_many :students
 end
