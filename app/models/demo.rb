62d4a1e69fbdc89d20a47e611f07eed498f201af
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 11:35:09 +0200

    Remove all the concerns in models

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,7 +1,6 @@
 class Demo < ApplicationRecord
-  # validates :name, presence: true, uniqueness: true
-  # validates :description, presence: true, length: { minimum: 10, maximum: 1000}
-  include Validatable
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000}
 
   has_one :sub_demo
 end
