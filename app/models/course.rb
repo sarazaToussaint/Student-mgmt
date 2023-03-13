883cbafe408b858886710d93907519fe4b76565e
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 20:39:58 +0200

    Fix some conrns errors in all models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ApplicationRecord
- validates :name, :description, presence: true
- validates :name, uniqueness: true
- validates :description, length: { minimum: 10, maximum: 1000}
+#  validates :name, :description, presence: true
+#  validates :name, uniqueness: true
+#  validates :description, length: { minimum: 10, maximum: 1000}
+include Validatable
 
  has_and_belongs_to_many :students
 end
