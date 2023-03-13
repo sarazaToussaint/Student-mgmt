b8ba78815fa47fc0744f8f2851de8a4b3d70da9e
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 20:41:30 +0200

    Fix some conrns errors in all models

--- app/models/concerns/validatable.rb
+++ app/models/concerns/validatable.rb
@@ -4,6 +4,7 @@ module Validatable
   include do
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true, length: { minimum: 10, maximum: 1000}
+
   end
     
 end
