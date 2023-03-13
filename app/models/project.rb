c89a5262423d2f8f4a3961935e204b2b284ef463
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 11:23:16 +0200

    Add the concern modele for course, project & demo

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
+  # validates :name, presence: true, uniqueness: true
+  # validates :description, presence: true, length: { minimum: 10, maximum: 1000}
+  include Validatable
+
   has_many :student_projects
   has_many :students, through: :student_projects
 end
