97b2bd905e5b9268837bb7380fef9b2f21aec45f
sarazaToussaint <tmsaraza@gmail.com>
2023-03-13 20:47:02 +0200

    Remove the unused codes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,5 @@
 class Course < ApplicationRecord
-#  validates :name, :description, presence: true
-#  validates :name, uniqueness: true
-#  validates :description, length: { minimum: 10, maximum: 1000}
-include Validatable
+  include Validatable
 
- has_and_belongs_to_many :students
+  has_and_belongs_to_many :students
 end
