eab609b8d3f92b99e57287a31ab758b041d4974c
sarazaToussaint <tmsaraza@gmail.com>
2023-03-17 21:48:25 +0200

    Make some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     private
 
     def print_display_message
-      puts "I'm an Inherited before action filters"
+      puts "I'm an Inherited before action filter"
     end
 
     def after_display_message
