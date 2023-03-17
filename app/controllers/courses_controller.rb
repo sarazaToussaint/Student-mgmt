0972002229deeff9cf7fde9fa102728e09abf825
sarazaToussaint <tmsaraza@gmail.com>
2023-03-17 21:47:57 +0200

    Remove the unused codes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -51,12 +51,11 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_course
       @course = Course.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def course_params
       params.require(:course).permit(:name, :description)
     end
