5db450e8afb7ff8d3d05b487e5d39f63f45e0ed6
sarazaToussaint <tmsaraza@gmail.com>
2023-03-17 21:49:24 +0200

    Make some changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,25 +1,20 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
 
-  # GET /projects or /projects.json
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1 or /projects/1.json
   def show
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
   end
 
-  # POST /projects or /projects.json
   def create
     @project = Project.new(project_params)
 
