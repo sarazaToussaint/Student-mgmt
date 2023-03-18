c935c532d4c19a89beb9e63983d073fd8aec191c
sarazaToussaint <tmsaraza@gmail.com>
2023-03-18 20:49:48 +0200

    Make some changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,9 +17,9 @@ class Student < ApplicationRecord
   def display_student_age
    if self.date_of_birth.present?
     age = Date.today.year - self.date_of_birth.year
-    puts "=========Age of the student is #{age}============="
+    puts "==========Age of the student is #{age}============="
    else
-    puts "========Age can not be calculated without date_of_birth========"
+    puts "========Age can not be calculated without date_of_birth=========="
   end 
   end
 
