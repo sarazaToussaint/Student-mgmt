7d484b57f004e20cdfd34b41e6e7835030725517
sarazaToussaint <tmsaraza@gmail.com>
2023-03-19 22:25:12 +0200

    Make some changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
 
   def index
-    @projects = Project.find_each
+    @projects = Project.all
   end
 
   def show
