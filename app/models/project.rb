130defbf9ed61b4642f17cde009ea43f14350c22
sarazaToussaint <tmsaraza@gmail.com>
2023-05-03 19:11:30 +0200

    Make a change in the project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   include Validatable
 
-  has_many :student_projects
-  has_many :students, through: :student_projects
+  has_many :student_project
+  has_many :students, through: :student_project
 end
