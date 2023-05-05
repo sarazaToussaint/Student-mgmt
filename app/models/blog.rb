012761b4a213c21fda96f6370ef7a44f1e81425c
sarazaToussaint <tmsaraza@gmail.com>
2023-05-05 21:48:35 +0200

    Make change in blog.rb file

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,8 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: true
-  validates :content, length: {minimum: 10, maximum: 1001 }
+  validates :content, length: {minimum: 10, maximum: 1000 }
 
    belongs_to :student
-   
+
 end
