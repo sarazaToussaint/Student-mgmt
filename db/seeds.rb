1159e0129b6b7dfffb9425c26809c57f3cdfda43
sarazaToussaint <tmsaraza@gmail.com>
2023-05-07 23:39:00 +0200

    Delete codes in the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,13 +25,3 @@ Course.create(name: "Angular Bootcamp", description: "Pending")
 Course.create(name: "NodeJS Bootcamp", description: "Pending")
 Course.create(name: "Java Bootcamp", description: "Pending")
 Course.create(name: "Python Bootcamp", description: "Pending")
-
-student = Student.create(
-  first_name: "Salomon",
-  last_name: "Babalao",
-  email: "sb@gmail.com",
-  date_of_birth: "11-03-2000",
-  local_address: "Sb",
-  permenant_address: "Bostone",
-  contact: 0784504044
-)
